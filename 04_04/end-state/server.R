56bbc50649aaaee7023e4c0835dd9fcedf6c448b
Shubham Jagtap <shubhamjagtap2126@gmail.com>
2021-04-08 06:44:55 +0530

    Building Data Apps with R and Shiny Essential Training = 04_04

---
+++ 04_04/end-state/server.R
@@ -0,0 +1,25 @@
+library("DT")
+library("tidyverse")
+function(input, output, session) {
+  
+  output$star_narrow <- renderDT({
+    starwars %>%
+      select(name, height, homeworld) %>%
+      arrange(desc(height))
+  }, rownames = FALSE)
+  
+  output$star_wide <- renderDT({
+    starwars %>%
+      select(name:homeworld) %>%
+      arrange(desc(height)) %>%
+      datatable(rownames = input$show_rownames,
+                extensions = "Responsive")
+  })
+  
+  output$star_list <- renderDT({
+    starwars %>%
+      select(name, films:starships) %>%
+      datatable(rownames = FALSE)
+  })
+  
+}
